54878e997e2b74b39b85f128632ff18cf908613f
sharman <sharmanfaridz@gmail.com>
2025-01-14 15:40:12 +0800

    move text node to html node into node handler

--- src/node_handler.py
+++ src/node_handler.py
@@ -1,4 +1,29 @@
 from textnode import TextType, TextNode
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextNode):
+        raise TypeError("text node is not the correct type")
+    
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            props = {
+                "src" : text_node.url,
+                "alt" : ""
+            }
+            return LeafNode("img", "", props)
+        case _:
+            raise Expection("invalid text node text type")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []
