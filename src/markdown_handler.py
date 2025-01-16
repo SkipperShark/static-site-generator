ce491da3ee3e9abab0c0ecb3de2e22925dc4888b
sharman <sharmanfaridz@gmail.com>
2025-01-16 13:40:18 +0800

    complete block to block type function

--- src/markdown_handler.py
+++ src/markdown_handler.py
@@ -1,5 +1,6 @@
 from pprint import pp
 from enum import Enum
+import re
 
 class MarkdownBlockTypes(Enum):
     PARAGRAPH = "paragraph"
@@ -20,4 +21,16 @@ def markdown_to_blocks(markdown) -> list[str]:
     return cleaned_blocks
     
 
-def block_to_block_type(markdown_block) -> str:
+def block_to_block_type(markdown_block: str) -> str:
+    if re.match(r"^#{1,6} ", markdown_block):
+        return MarkdownBlockTypes.HEADING
+    elif markdown_block.startswith("```") and markdown_block.endswith("```"):
+        return MarkdownBlockTypes.CODE
+    elif markdown_block.startswith(">"):
+        return MarkdownBlockTypes.QUOTE
+    elif markdown_block.startswith("* ") or markdown_block.startswith("- "):
+        return MarkdownBlockTypes.UNORDERED_LIST
+    elif re.match(r"^\d*. ", markdown_block):
+        return MarkdownBlockTypes.ORDERED_LIST
+
+    return MarkdownBlockTypes.PARAGRAPH
--- src/test_markdown_handler.py
+++ src/test_markdown_handler.py
@@ -1,79 +1,134 @@
 import unittest
-from markdown_handler import markdown_to_blocks
+from markdown_handler import (
+    markdown_to_blocks, block_to_block_type, MarkdownBlockTypes as mdBlockTypes
+)
 
-HEADING_BLOCK = "# This is a heading"
+HEADING_1_BLOCK = "# This is a heading1"
+HEADING_2_BLOCK = "## This is a heading2"
 PARAGRAPH_BLOCK = "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
-LIST_BLOCK = "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+UNORDERED_LIST_BLOCK_1 = "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+UNORDERED_LIST_BLOCK_2 = "- This is the first list item in a list block\n- This is a list item\n- This is another list item"
+CODE_BLOCK = "```i am a code block```"
+CODE_BLOCK_INVALID = "```i am a code block"
+QUOTE_BLOCK = ">I am a quote"
+ORDERED_LIST_BLOCK_IN_ORDER = "1. First item\n2. 2nd item"
+ORDERED_LIST_BLOCK_OUT_OF_ORDER = "1. First item\n3. 3RD item"
 
 class TestMarkDownToBlocks(unittest.TestCase):
     
     def test_1_block_1_lines(self):
         self.assertEqual(
-            markdown_to_blocks(HEADING_BLOCK),
-            [HEADING_BLOCK]
+            markdown_to_blocks(HEADING_1_BLOCK),
+            [HEADING_1_BLOCK]
         )
         
     
     def test_1_block_with_2_lines(self):
         self.assertEqual(
-            markdown_to_blocks(f"{HEADING_BLOCK}\n{HEADING_BLOCK}"),
-            [f"{HEADING_BLOCK}\n{HEADING_BLOCK}"]
+            markdown_to_blocks(f"{HEADING_1_BLOCK}\n{HEADING_1_BLOCK}"),
+            [f"{HEADING_1_BLOCK}\n{HEADING_1_BLOCK}"]
         )
         
         
     def test_2_blocks_each_with_1_line(self):
         self.assertEqual(
-            markdown_to_blocks(f"{HEADING_BLOCK}\n\n{PARAGRAPH_BLOCK}"),
-            [HEADING_BLOCK, PARAGRAPH_BLOCK]
+            markdown_to_blocks(f"{HEADING_1_BLOCK}\n\n{PARAGRAPH_BLOCK}"),
+            [HEADING_1_BLOCK, PARAGRAPH_BLOCK]
         )
         
     
     def test_2_blocks_one_block_with_1_line_one_block_with_multiple_lines(self):
         self.assertEqual(
-            markdown_to_blocks(f"{HEADING_BLOCK}\n\n{LIST_BLOCK}"),
-            [HEADING_BLOCK, LIST_BLOCK]
+            markdown_to_blocks(f"{HEADING_1_BLOCK}\n\n{UNORDERED_LIST_BLOCK_1}"),
+            [HEADING_1_BLOCK, UNORDERED_LIST_BLOCK_1]
         )
         
         
     def test_2_blocks_both_with_multiple_lines(self):
         self.assertEqual(
-            markdown_to_blocks(f"{LIST_BLOCK}\n\n{LIST_BLOCK}"),
-            [LIST_BLOCK, LIST_BLOCK]
+            markdown_to_blocks(f"{UNORDERED_LIST_BLOCK_1}\n\n{UNORDERED_LIST_BLOCK_1}"),
+            [UNORDERED_LIST_BLOCK_1, UNORDERED_LIST_BLOCK_1]
         )
         
         
     def test_3_blocks(self):
         self.assertEqual(
-            markdown_to_blocks(f"{HEADING_BLOCK}\n\n{PARAGRAPH_BLOCK}\n\n{LIST_BLOCK}"),
-            [HEADING_BLOCK, PARAGRAPH_BLOCK, LIST_BLOCK]
+            markdown_to_blocks(f"{HEADING_1_BLOCK}\n\n{PARAGRAPH_BLOCK}\n\n{UNORDERED_LIST_BLOCK_1}"),
+            [HEADING_1_BLOCK, PARAGRAPH_BLOCK, UNORDERED_LIST_BLOCK_1]
         )
         
     
     def test_2_blocks_with_3_blank_lines_between_them(self):
         self.assertEqual(
-            markdown_to_blocks(f"{HEADING_BLOCK}\n\n\n\n{PARAGRAPH_BLOCK}"),
-            [HEADING_BLOCK, PARAGRAPH_BLOCK]
+            markdown_to_blocks(f"{HEADING_1_BLOCK}\n\n\n\n{PARAGRAPH_BLOCK}"),
+            [HEADING_1_BLOCK, PARAGRAPH_BLOCK]
         )
         
 
 class TestBlockToBlockType(unittest.TestCase):
-    pass
+    
+    def test_heading_1(self):
+        self.assertEqual(
+            block_to_block_type(HEADING_1_BLOCK), mdBlockTypes.HEADING
+        )
+        
+        
+    def test_heading_2(self):
+        self.assertEqual(
+            block_to_block_type(HEADING_2_BLOCK), mdBlockTypes.HEADING
+        )
 
-    #testcases
-    # heading1
-    # heading2
-    # heading3
-    # heading4
-    # heading5
-    # heading6
-    # code blocks
-    # quote block
-    # unordered list
-        # test 1 item
-        # test 2 items
-        # test 3 items
-    # ordered list
-        # test 1 item
-        # test 2 items
-        # test 3 items
-    # else -> normal paragraph
+
+    def test_code_block_valid(self):
+        self.assertEqual(
+            block_to_block_type(CODE_BLOCK), mdBlockTypes.CODE
+        )
+
+
+    def test_code_block_invalid(self):
+        self.assertEqual(
+            block_to_block_type(CODE_BLOCK_INVALID),
+            mdBlockTypes.PARAGRAPH
+        )
+
+
+    def test_quote_block(self):
+        self.assertEqual(
+            block_to_block_type(QUOTE_BLOCK), mdBlockTypes.QUOTE
+        )
+        
+
+    def test_unordered_list_asterisk(self):
+        self.assertEqual(
+            block_to_block_type(UNORDERED_LIST_BLOCK_1),
+            mdBlockTypes.UNORDERED_LIST
+        )
+
+
+    def test_unordered_list_dash(self):
+        self.assertEqual(
+            block_to_block_type(UNORDERED_LIST_BLOCK_2),
+            mdBlockTypes.UNORDERED_LIST
+        )
+        
+        
+    def test_ordered_list_in_order(self):
+        self.assertEqual(
+            block_to_block_type(ORDERED_LIST_BLOCK_IN_ORDER),
+            mdBlockTypes.ORDERED_LIST
+        )
+        
+        
+    def test_ordered_list_out_of_order(self):
+        self.assertEqual(
+            block_to_block_type(ORDERED_LIST_BLOCK_OUT_OF_ORDER),
+            mdBlockTypes.ORDERED_LIST
+        )
+        
+        
+    def test_paragraph(self):
+        self.assertEqual(
+            block_to_block_type(PARAGRAPH_BLOCK),
+            mdBlockTypes.PARAGRAPH
+        )
+    
