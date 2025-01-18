84e121ff98f9c6d869f550a6c95b419f8ac96a61
sharman <sharmanfaridz@gmail.com>
2025-01-18 10:26:14 +0800

    move main code into main() function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from textnode import TextNode
 
-test = TextNode("This is a text node", "hello", "https://www.boot.dev")
-print(test)
+def main():
+    test = TextNode("This is a text node", "hello", "https://www.boot.dev")
+    print(test)
+    
+main()
