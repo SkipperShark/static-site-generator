07bdbd3b5bdb0a916ccd8028a81a3e2b6f37e10b
sharman <sharmanfaridz@gmail.com>
2025-01-21 14:36:24 +0800

    modify block_to_block_type according to solution files

--- src/markdown_handler.py
+++ src/markdown_handler.py
@@ -13,19 +13,52 @@ class MarkdownBlockTypes(Enum):
 
 def markdown_to_blocks(markdown) -> list[str]:
     blocks = markdown.split("\n\n")
-    return [block.strip.strip().strip("\n") for block in blocks if block != '']
+    return [block.strip().strip("\n") for block in blocks if block != '']
 
 
-def block_to_block_type(markdown_block: str) -> str:
-    if re.match(r"^#{1,6} ", markdown_block):
+# def block_to_block_type(markdown_block: str) -> str:
+#     if re.match(r"^#{1,6} ", markdown_block):
+#         return MarkdownBlockTypes.HEADING
+#     elif markdown_block.startswith("```") and markdown_block.endswith("```"):
+#         return MarkdownBlockTypes.CODE
+#     elif markdown_block.startswith(">"):
+#         return MarkdownBlockTypes.QUOTE
+#     elif markdown_block.startswith("* ") or markdown_block.startswith("- "):
+#         return MarkdownBlockTypes.UNORDERED_LIST
+#     elif re.match(r"^\d*. ", markdown_block):
+#         return MarkdownBlockTypes.ORDERED_LIST
+
+#     return MarkdownBlockTypes.PARAGRAPH
+
+def block_to_block_type(block: str) -> str:
+    lines = block.split("\n")
+    if re.match(r"^#{1,6} ", block):
         return MarkdownBlockTypes.HEADING
-    elif markdown_block.startswith("```") and markdown_block.endswith("```"):
+
+    elif block.startswith("```") and block.endswith("```"):
         return MarkdownBlockTypes.CODE
-    elif markdown_block.startswith(">"):
+
+    elif block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return MarkdownBlockTypes.PARAGRAPH
+            continue
         return MarkdownBlockTypes.QUOTE
-    elif markdown_block.startswith("* ") or markdown_block.startswith("- "):
+
+    elif block.startswith("* ") or block.startswith("- "):
+        for line in lines:
+            if not line.startswith("* ") and not line.startswith("- "):
+                return MarkdownBlockTypes.PARAGRAPH
+            continue
         return MarkdownBlockTypes.UNORDERED_LIST
-    elif re.match(r"^\d*. ", markdown_block):
+
+
+    elif re.match(r"^\d*. ", block):
+        for line in lines:
+            if not re.match(r"^\d*. ", block):
+                return MarkdownBlockTypes.PARAGRAPH
+            continue
         return MarkdownBlockTypes.ORDERED_LIST
 
     return MarkdownBlockTypes.PARAGRAPH
+    
