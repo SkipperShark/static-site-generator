e97b9bf7a365f8c531ffbd030b3be80c04947963
sharman <sharmanfaridz@gmail.com>
2025-01-21 16:31:32 +0800

    start main control logic of markdown_to_html_node

--- src/markdown_handler.py
+++ src/markdown_handler.py
@@ -2,6 +2,8 @@ from pprint import pp
 from enum import Enum
 import re
 
+from node_handler import text_to_textnode, text_node_to_html_node
+
 class MarkdownBlockTypes(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -11,26 +13,12 @@ class MarkdownBlockTypes(Enum):
     ORDERED_LIST = "ordered list"
 
 
-def markdown_to_blocks(markdown) -> list[str]:
+def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     return [block.strip().strip("\n") for block in blocks if block != '']
 
 
-# def block_to_block_type(markdown_block: str) -> str:
-#     if re.match(r"^#{1,6} ", markdown_block):
-#         return MarkdownBlockTypes.HEADING
-#     elif markdown_block.startswith("```") and markdown_block.endswith("```"):
-#         return MarkdownBlockTypes.CODE
-#     elif markdown_block.startswith(">"):
-#         return MarkdownBlockTypes.QUOTE
-#     elif markdown_block.startswith("* ") or markdown_block.startswith("- "):
-#         return MarkdownBlockTypes.UNORDERED_LIST
-#     elif re.match(r"^\d*. ", markdown_block):
-#         return MarkdownBlockTypes.ORDERED_LIST
-
-#     return MarkdownBlockTypes.PARAGRAPH
-
-def block_to_block_type(block: str) -> str:
+def block_to_block_type(block: str):
     lines = block.split("\n")
     if re.match(r"^#{1,6} ", block):
         return MarkdownBlockTypes.HEADING
@@ -61,4 +49,49 @@ def block_to_block_type(block: str) -> str:
         return MarkdownBlockTypes.ORDERED_LIST
 
     return MarkdownBlockTypes.PARAGRAPH
+
+def markdown_to_html_node(markdown: str):
+    blocks = markdown_to_blocks(markdown)
+    print("block")
+    print(blocks)
+
+    for block in blocks:
+        print("\n--------------- new iteration of block")
+        print("block")
+        print(block)
+        block_type = block_to_block_type(block)
+        print(f"block type : {block_type}")
+        
+        if block_type == MarkdownBlockTypes.PARAGRAPH:
+            text_nodes = text_to_textnode(block)
+            print("text nodes")
+            print(text_nodes)
+            
+            
+    return
+
+f = open("sample_md.md", "r+")
+print(markdown_to_html_node(f.read()))
+
+
+    # split the markdown into blocks
+    # for each block
+    # determine the type of block
+    # split the text of the block into text nodes using (text_to_textnode)
+    # for each textnode, convert them into leaf nodes
+    # depending on the type of block, create the appropriate parent html node
+    # then, put all the leaf nodes as children of the parent html node
     
+    # all blocks should be under a parent html node (which is a div)
+    
+    #Split the markdown into blocks (you already have a function for this)
+    #Loop over each block:
+    #Determine the type of block (you already have a function for this)
+    #Based on the type of block, create a new HTMLNode with the proper data
+    #Assign the proper child HTMLNode objects to the block node. I created a
+    # shared text_to_children(text) function that works for all block types.
+    # It takes a string of text and returns a list of HTMLNodes that represent
+    # the inline markdown using previously created functions
+    # (think TextNode -> HTMLNode).
+    #Make all the block nodes children under a single parent HTML node
+    # (which should just be a div) and return it.
