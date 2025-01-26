81ce3656124f506740e0bf99c37df58567033e17
sharman <sharmanfaridz@gmail.com>
2025-01-26 17:18:51 +0800

    create recursive generate_page function

--- src/generate_page.py
+++ src/generate_page.py
@@ -47,3 +47,45 @@ def generate_page(from_path, template_path, dest_path):
         
     f = open(dest_path, "w")
     f.write(html)
+
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    print("\nstart of generate_page_recursive")
+    print(f"dir_path_content : {dir_path_content}")
+    print(f"template_path : {template_path}")
+    print(f"dest_dir_path : {dest_dir_path}")
+
+    if os.path.isfile(dir_path_content):
+        print(f"writing page from : {dir_path_content} to {dest_dir_path}")
+        generate_page(dir_path_content, template_path, dest_dir_path)
+        return
+    
+    print("\nloop started\n")
+    for path in os.listdir(dir_path_content):
+        print(f"directory found!")
+        generate_page_recursive(
+            dir_path_content = os.path.join(dir_path_content, path),
+            template_path = template_path,
+            dest_dir_path = os.path.join(dest_dir_path, path)
+        )
+        
+    
+    # md_f = open(from_path, "r")
+    # md_data = md_f.read()
+    
+    # template_f = open(template_path, "r")
+    # template_data = template_f.read()
+    
+    # html_node = markdown_to_html_node(md_data)
+    # content = html_node.to_html()
+    
+    # title = extract_title(md_data)
+    
+    # html = template_data.replace(r"{{ Title }}", title) \
+    #     .replace(r"{{ Content }}", content)
+        
+    # if not os.path.exists(os.path.dirname(dest_path)):
+    #     os.makedirs(os.path.dirname(dest_path))
+        
+    # f = open(dest_path, "w")
+    # f.write(html)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
 import shutil
-from copystatic import copy_files
-from generate_page import generate_page
+from copystatic import copy_files_recursive
+from generate_page import generate_page, generate_page_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
 path_template = "./template.html"
 path_content_index = "./content/index.md"
 path_public_index = "./public/index.html"
@@ -16,8 +17,12 @@ def main():
         shutil.rmtree(dir_path_public)
         
     print("Copying static files to public directory...")
-    copy_files(dir_path_static, dir_path_public)
-    generate_page(path_content_index, path_template, path_public_index)
-    
+    copy_files_recursive(dir_path_static, dir_path_public)
+    # generate_page(path_content_index, path_template, path_public_index)
+    generate_page_recursive(
+        dir_path_content = dir_path_content,
+        template_path = path_template,
+        dest_dir_path = dir_path_public
+    )
 
 main()
